26254bed003f72782d72d20093619f5943b367a0
doctorm4id <doctorm4id@gmail.com>
2026-08-01 21:20:47 -0700

    Put neoforge imports under neoforge territory.

--- src/main/kotlin/dev/doctorm4id/rot/platform/neoforge/NeoforgeClientEventSubscriber.kt
+++ src/main/kotlin/dev/doctorm4id/rot/platform/neoforge/NeoforgeClientEventSubscriber.kt
@@ -1,14 +1,14 @@
 package dev.doctorm4id.rot.platform.neoforge
 
-import dev.doctorm4id.rot.TheRot
+//? neoforge {
+
+/*import dev.doctorm4id.rot.TheRot
 import net.neoforged.api.distmarker.Dist
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
 
-//? neoforge {
-
-/*@EventBusSubscriber(modid = TheRot.MOD_ID, value = [Dist.CLIENT])
+@EventBusSubscriber(modid = TheRot.MOD_ID, value = [Dist.CLIENT])
 object NeoforgeClientEventSubscriber {
 
 	@JvmStatic
