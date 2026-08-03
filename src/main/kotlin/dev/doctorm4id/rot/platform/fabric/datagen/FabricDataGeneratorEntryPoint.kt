5d2e0f9ed3b394ac86ac025689b24341c2460a3d
doctorm4id <doctorm4id@gmail.com>
2026-08-03 13:32:41 -0700

    Add FabricDataGeneratorEntryPoint.kt

--- src/main/kotlin/dev/doctorm4id/rot/platform/fabric/datagen/FabricDataGeneratorEntryPoint.kt
+++ src/main/kotlin/dev/doctorm4id/rot/platform/fabric/datagen/FabricDataGeneratorEntryPoint.kt
@@ -1,5 +1,18 @@
 package dev.doctorm4id.rot.platform.fabric.datagen
 
-// Will do this later, I don't fully understand the ModRecipeProvider template.
-class FabricDataGeneratorEntryPoint {
+//? fabric {
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+// I'm thinking that I can have and run datagen on fabric and not need datagen for neoforge.
+class FabricDataGeneratorEntryPoint : DataGeneratorEntrypoint {
+
+	override fun onInitializeDataGenerator(generator: FabricDataGenerator) {
+		val pack = generator.createPack()
+
+		pack.addProvider(::ModModelProvider)
+	}
 }
+
+//?}
